11d3654cb8dda422e67646694409eb8f0266df07
amalipalal <palal.asare@amalitech.com>
2025-12-10 15:02:39 +0000

    feat: update menu display in DisplayUtil

--- src/main/java/utils/DisplayUtil.java
+++ src/main/java/utils/DisplayUtil.java
@@ -36,8 +36,9 @@ public class DisplayUtil {
         System.out.println("2. View Accounts");
         System.out.println("3. Process Transaction");
         System.out.println("4. View Transaction History");
-        System.out.println("5. Run tests");
-        System.out.println("6. Exit");
+        System.out.println("5. Save Data");
+        System.out.println("6. Run tests");
+        System.out.println("7. Exit");
         System.out.println();
     }
 
