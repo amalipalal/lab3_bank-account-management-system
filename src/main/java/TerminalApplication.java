8fdd85268776a1b342b40461471a6b9aa1b21ca9
amalipalal <palal.asare@amalitech.com>
2025-12-10 13:52:17 +0000

    feat: update TerminalApplication to load stored data
    - update TerminalApplication constructor with FileStorageService
    - update AccountManager object creation
    - add loadSavedAccounts method for load on app start

--- src/main/java/TerminalApplication.java
+++ src/main/java/TerminalApplication.java
@@ -1,7 +1,11 @@
+import config.AppConfig;
 import handlers.AccountFlowHandler;
 import handlers.TestHandler;
 import handlers.TransactionFlowHandler;
+import interfaces.DataStorageService;
+import models.Account;
 import services.AccountManager;
+import services.FileStorageService;
 import services.TransactionManager;
 import services.BankingService;
 import utils.DataSeeder;
@@ -10,9 +14,13 @@ import utils.InputReader;
 import utils.id.AccountIdGenerator;
 import utils.id.TransactionIdGenerator;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TerminalApplication {
+    private final DataStorageService dataStorageService;
     private final BankingService bankingService;
     private final InputReader input;
     private final AccountFlowHandler accountFlowHandler;
@@ -20,14 +28,29 @@ public class TerminalApplication {
     private final TestHandler testHandler;
 
     public TerminalApplication() {
+        this.dataStorageService = new FileStorageService(
+                AppConfig.ACC_STORE_FILE_NAME,
+                AppConfig.TRANS_STORE_FILE_NAME
+        );
+        Map<String, Account> savedAccounts = loadSavedAccounts(dataStorageService);
+
         this.bankingService = new BankingService(
-                new AccountManager(new AccountIdGenerator()),
+                new AccountManager(new AccountIdGenerator(), savedAccounts),
                 new TransactionManager(new TransactionIdGenerator())
         );
         this.input = new InputReader(new Scanner(System.in));
         this.accountFlowHandler = new AccountFlowHandler(bankingService, input);
         this.transactionFlowHandler = new TransactionFlowHandler(bankingService, input);
         this.testHandler = new TestHandler();
+    }
+
+    private Map<String, Account> loadSavedAccounts(DataStorageService store) {
+        try {
+            return store.loadAccounts();
+        } catch (IOException e) {
+            DisplayUtil.displayNotice(e.getMessage());
+            return new HashMap<>();
+        }
     }
 
     public void start() {
