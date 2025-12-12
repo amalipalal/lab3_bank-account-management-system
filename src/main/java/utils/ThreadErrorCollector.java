3efea7ea761a23388bce22a75b4a05c23fe478d3
amalipalal <palal.asare@amalitech.com>
2025-12-12 13:00:08 +0000

    feat: add error counter and reset
    This was added to help with the ui display for number of successful threads

--- src/main/java/utils/ThreadErrorCollector.java
+++ src/main/java/utils/ThreadErrorCollector.java
@@ -5,10 +5,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class ThreadErrorCollector {
+        private int PREVIOUS_ERROR_COUNT = 0;
+
         private final List<String> errors = Collections.synchronizedList(new ArrayList<>());
 
         public void addError(String msg) {
             errors.add(msg);
+            PREVIOUS_ERROR_COUNT += 1;
         }
 
         public List<String> getErrors() {
@@ -21,5 +24,17 @@ public class ThreadErrorCollector {
 
         public void showErrors() {
             errors.forEach(DisplayUtil::displayNotice);
+        }
+
+        public void resetCount() {
+            PREVIOUS_ERROR_COUNT = 0;
+        }
+
+        public int getPREVIOUS_ERROR_COUNT() {
+            return PREVIOUS_ERROR_COUNT;
+        }
+
+        public void clearErrors() {
+            this.errors.clear();
         }
 }
