20280a4f5372f54017cabba250e0207351d3ff0c
palalasare <palal.asare@amalitech.com>
2025-12-13 07:11:07 +0000

    feat: add app context to Main call

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        new TerminalApplication().start();
+        new TerminalApplication(new ApplicationContext()).start();
     }
 }
